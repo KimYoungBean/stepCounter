0f3f38ea443073a88f3d5d25f0cfbcf6d2af19a1
KyuJeong <heo0414@naver.com>
2018-01-17 20:37:53 +0900

    Add method that draw a path on the screen

--- app/src/main/java/com/example/kimyoungbin/stepcounter/MainActivity.java
+++ app/src/main/java/com/example/kimyoungbin/stepcounter/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.kimyoungbin.stepcounter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -37,6 +48,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
   private final double handTypingThs = 0.5;
 
   private int stepCount;
+  private int nCnt;
+  private int neCnt;
+  private int eCnt;
+  private int seCnt;
+  private int sCnt;
+  private int swCnt;
+  private int wCnt;
+  private int nwCnt;
+  private int dir;
 
   // Using the Accelometer & Gyroscoper
   private SensorManager mSensorManager = null;
@@ -53,7 +73,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
   private SensorEventListener mClsLis;
   private Sensor mClsSensor = null;
 
+  // Using the Dirsensor
+  private SensorEventListener mDirLis;
+  private Sensor mDirSensor = null;
+
   private TextView mTextView;
+  private TextView resultView[] = new TextView[8];
+
+
+  // Value
+  private int firstValue;
+  private int startValue;
+  private int lastValue;
+  private boolean isStart;
 
   // To distinguish state
   private boolean isPocket;
@@ -67,6 +99,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
   private long startTime;
   private long endTime;
 
+  // View information
+  private int displayWidth;
+  private int displayHeight;
+  private int oneStepWidth;
+  private int oneStepHeight;
+  private int stepCheck;
+
   @Override
   protected void onResume() {
     super.onResume();
@@ -79,6 +118,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     setContentView(R.layout.activity_main);
 
     mTextView = (TextView) findViewById(R.id.tv_count);
+    resultView[0] = (TextView)findViewById(R.id.tv_n);
+    resultView[1] = (TextView)findViewById(R.id.tv_ne);
+    resultView[2] = (TextView)findViewById(R.id.tv_e);
+    resultView[3] = (TextView)findViewById(R.id.tv_se);
+    resultView[4] = (TextView)findViewById(R.id.tv_s);
+    resultView[5] = (TextView)findViewById(R.id.tv_sw);
+    resultView[6] = (TextView)findViewById(R.id.tv_w);
+    resultView[7] = (TextView)findViewById(R.id.tv_nw);
+
+    dir = 0;
+    nCnt=0;
+    neCnt=0;
+    eCnt=0;
+    seCnt=0;
+    sCnt=0;
+    swCnt=0;
+    wCnt=0;
+    nwCnt=0;
 
     isPush = true;
     pocketFlag = true;
@@ -86,17 +143,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     handTypingFlag = true;
     isPocket = false;
     stepCount = 0;
+    isStart = false;
 
-    //Using the Gyroscope & Accelometer
+    //Using the Sensors
     mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
     //Using the Accelometer
     mAccelometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    mAccLis = new AccelometerListener();
 
     //Using the Gyroscoper
     mGgyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-    mGyroLis = new GyroscopeListener();
+
+    //Using the DirSensor
+    mDirSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
 
     //Using the Closesensor
     mClsSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -121,36 +180,123 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
       }
     };
 
+    DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+
+    displayWidth = dm.widthPixels;
+    displayHeight = dm.heightPixels;
+
+    oneStepWidth = displayWidth / 27;
+    oneStepHeight = displayHeight / 67;
+
+    final ViewEx viewEx = new ViewEx(this);
+
     //Touch Listener for Accelometer
-    findViewById(R.id.a_start).setOnTouchListener(new View.OnTouchListener() {
-      @Override
-      public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-
-          case MotionEvent.ACTION_DOWN:
-            if (isPush) {
-              mSensorManager.registerListener(mAccLis, mAccelometerSensor, SensorManager.SENSOR_DELAY_UI);
-              mSensorManager.registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
-              mSensorManager.registerListener(mClsLis, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_FASTEST);
-              isPush = false;
-            } else {
-              mSensorManager.unregisterListener(mAccLis);
-              mSensorManager.unregisterListener(mClsLis);
-              isPush = true;
-            }
-
-            break;
-
-          /*
-          case MotionEvent.ACTION_UP:
-            mSensorManager.unregisterListener(mAccLis);
-            break;*/
-
+    findViewById(R.id.a_start).setOnClickListener(new Button.OnClickListener(){
+      public void onClick(View v){
+        if (isPush) {
+          mDirLis = new mDirectionListener();
+          mAccLis = new AccelometerListener();
+          mGyroLis = new GyroscopeListener();
+          isStart = true;
+          mSensorManager.registerListener(mAccLis, mAccelometerSensor, SensorManager.SENSOR_DELAY_UI);
+          mSensorManager.registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+          mSensorManager.registerListener(mClsLis, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_FASTEST);
+          mSensorManager.registerListener(mDirLis, mDirSensor, SensorManager.SENSOR_DELAY_NORMAL);
+          isPush = false;
+          setContentView(viewEx);
+        } else {
+          mSensorManager.unregisterListener(mAccLis);
+          mSensorManager.unregisterListener(mClsLis);
+          mSensorManager.unregisterListener(mDirLis);
+          isStart = false;
+          isPush = true;
         }
-        return false;
       }
     });
 
+  }
+
+  protected class ViewEx extends View {
+
+    private Bitmap img;
+    int imgWidth;
+    int imgHeight;
+    int curWidth;
+    int curHeight;
+    double doubleCurWidth;
+    double doubleCurHeight;
+    int sizeWidth;
+    int sizeHeight;
+    Rect dst;
+    Path path;
+
+    public ViewEx(Context context) {
+      super(context);
+      setBackgroundColor(Color.WHITE);
+      Resources r = context.getResources();
+      path = new Path();
+      img = BitmapFactory.decodeResource(r, R.drawable.ic_navigation_black_24dp);
+      imgWidth = img.getWidth();
+      imgHeight = img.getHeight();
+
+      curWidth = displayWidth / 2;
+      curHeight = displayHeight / 2;
+
+      /* length of one side */
+      sizeWidth = imgWidth / 4;
+      sizeHeight = imgHeight / 4;
+
+      stepCheck = 1;
+
+      mHandler.sendEmptyMessageDelayed(0, 10);
+    }
+
+    public void onDraw(Canvas canvas) {
+
+      Paint MyPaint = new Paint();
+      MyPaint.setStrokeWidth(5f);
+      MyPaint.setStyle(Paint.Style.STROKE);
+      MyPaint.setColor(Color.RED);
+
+      /* initial location */
+      path.moveTo(curWidth, curHeight);
+
+      /* calculate direction and draw on screen */
+      if(stepCheck == stepCount) {
+        doubleCurWidth = curWidth + (oneStepWidth * Math.sin(Math.toRadians(lastValue)));
+        doubleCurHeight = curHeight - (oneStepHeight * Math.cos(Math.toRadians(lastValue)));
+
+        curWidth = (int)doubleCurWidth;
+        curHeight = (int)doubleCurHeight;
+
+        stepCheck++;
+      }
+
+      /* draw path in a line */
+      path.lineTo(curWidth, curHeight);
+
+      /* rotate image angle */
+      Matrix matrix = new Matrix();
+      matrix.postRotate(lastValue);
+      Bitmap newImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+
+      /* image size, location setting */
+      dst = new Rect(curWidth - sizeWidth, curHeight - sizeHeight, curWidth + sizeWidth, curHeight + sizeHeight);
+      canvas.drawBitmap(newImg, null, dst, null);
+
+      canvas.drawPath(path, MyPaint);
+      super.onDraw(canvas);
+    }
+
+    Handler mHandler=new Handler(){
+
+      public void handleMessage(Message msg)
+      {
+        invalidate();
+        mHandler.sendEmptyMessageDelayed(0, 10);
+      }
+
+    };
   }
 
   @Override
@@ -159,6 +305,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Log.e("LOG", "onPause()");
     mSensorManager.unregisterListener(mAccLis);
     mSensorManager.unregisterListener(mClsLis);
+    mSensorManager.unregisterListener(mDirLis);
   }
 
   @Override
@@ -167,6 +314,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Log.e("LOG", "onDestroy()");
     mSensorManager.unregisterListener(mAccLis);
     mSensorManager.unregisterListener(mClsLis);
+    mSensorManager.unregisterListener(mDirLis);
   }
 
   @Override
@@ -182,8 +330,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
   private class AccelometerListener implements SensorEventListener {
 
+
     @Override
     public void onSensorChanged(SensorEvent event) {
+      String str;
 
       if(startTime == 0)
         startTime = event.timestamp;
@@ -215,6 +365,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
       if (isPocket) {
         if (E > minPocketThs && E < maxPocketThs && pocketFlag && isPocketToHand) {
           stepCount++;
+          switch (dir){
+            case 0:
+              nCnt++;
+              str = ""+nCnt;
+              resultView[0].setText(str);
+              break;
+            case 1:
+              neCnt++;
+              str = ""+neCnt;
+              resultView[1].setText(str);
+              break;
+            case 2:
+              eCnt++;
+              str = ""+eCnt;
+              resultView[2].setText(str);
+              break;
+            case 3:
+              seCnt++;
+              str = ""+seCnt;
+              resultView[3].setText(str);
+              break;
+            case 4:
+              sCnt++;
+              str = ""+sCnt;
+              resultView[4].setText(str);
+              break;
+            case 5:
+              swCnt++;
+              str = ""+swCnt;
+              resultView[5].setText(str);
+              break;
+            case 6:
+              wCnt++;
+              str = ""+wCnt;
+              resultView[6].setText(str);
+              break;
+            case 7:
+              nwCnt++;
+              str = ""+nwCnt;
+              resultView[7].setText(str);
+              break;
+            default:
+              break;
+          }
           Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
           Log.e("LOG", String.valueOf(stepCount));
           pocketFlag = false;
@@ -230,8 +424,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         /** Calling **/
         else if (E > minCallingThs && E < maxCallingThs && callingFlag && isPocketToHand) {
           stepCount++;
-          Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
-          Log.e("LOG", String.valueOf(stepCount));
+          switch (dir){
+            case 0:
+              nCnt++;
+              str = ""+nCnt;
+              resultView[0].setText(str);
+              break;
+            case 1:
+              neCnt++;
+              str = ""+neCnt;
+              resultView[1].setText(str);
+              break;
+            case 2:
+              eCnt++;
+              str = ""+eCnt;
+              resultView[2].setText(str);
+              break;
+            case 3:
+              seCnt++;
+              str = ""+seCnt;
+              resultView[3].setText(str);
+              break;
+            case 4:
+              sCnt++;
+              str = ""+sCnt;
+              resultView[4].setText(str);
+              break;
+            case 5:
+              swCnt++;
+              str = ""+swCnt;
+              resultView[5].setText(str);
+              break;
+            case 6:
+              wCnt++;
+              str = ""+wCnt;
+              resultView[6].setText(str);
+              break;
+            case 7:
+              nwCnt++;
+              str = ""+nwCnt;
+              resultView[7].setText(str);
+              break;
+            default:
+              break;
+          }
+          //Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
+          //Log.e("LOG", String.valueOf(stepCount));
           callingFlag = false;
 
           Handler mHandler = new Handler();
@@ -247,8 +485,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         /** Walking with typing **/
         if (E > minTypingThs && E < maxTypingThs && isHandTyping && handTypingFlag && isPocketToHand) {
           stepCount++;
-          Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
-          Log.e("LOG", String.valueOf(stepCount));
+          switch (dir){
+            case 0:
+              nCnt++;
+              str = ""+nCnt;
+              resultView[0].setText(str);
+              break;
+            case 1:
+              neCnt++;
+              str = ""+neCnt;
+              resultView[1].setText(str);
+              break;
+            case 2:
+              eCnt++;
+              str = ""+eCnt;
+              resultView[2].setText(str);
+              break;
+            case 3:
+              seCnt++;
+              str = ""+seCnt;
+              resultView[3].setText(str);
+              break;
+            case 4:
+              sCnt++;
+              str = ""+sCnt;
+              resultView[4].setText(str);
+              break;
+            case 5:
+              swCnt++;
+              str = ""+swCnt;
+              resultView[5].setText(str);
+              break;
+            case 6:
+              wCnt++;
+              str = ""+wCnt;
+              resultView[6].setText(str);
+              break;
+            case 7:
+              nwCnt++;
+              str = ""+nwCnt;
+              resultView[7].setText(str);
+              break;
+            default:
+              break;
+          }
+          //Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
+          //Log.e("LOG", String.valueOf(stepCount));
           isHandTyping = false;
           handTypingFlag = false;
 
@@ -262,9 +544,53 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         /** Hand held working **/
         else if (E > minHeldThs && E < maxHeldThs && isHandHeld && handHeldFlag) {
           stepCount++;
+          switch (dir){
+            case 0:
+              nCnt++;
+              str = ""+nCnt;
+              resultView[0].setText(str);
+              break;
+            case 1:
+              neCnt++;
+              str = ""+neCnt;
+              resultView[1].setText(str);
+              break;
+            case 2:
+              eCnt++;
+              str = ""+eCnt;
+              resultView[2].setText(str);
+              break;
+            case 3:
+              seCnt++;
+              str = ""+seCnt;
+              resultView[3].setText(str);
+              break;
+            case 4:
+              sCnt++;
+              str = ""+sCnt;
+              resultView[4].setText(str);
+              break;
+            case 5:
+              swCnt++;
+              str = ""+swCnt;
+              resultView[5].setText(str);
+              break;
+            case 6:
+              wCnt++;
+              str = ""+wCnt;
+              resultView[6].setText(str);
+              break;
+            case 7:
+              nwCnt++;
+              str = ""+nwCnt;
+              resultView[7].setText(str);
+              break;
+            default:
+              break;
+          }
           isHandHeld = false;
-          Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
-          Log.e("LOG", String.valueOf(stepCount));
+          //Log.e("LOG", "ACCELOMETER           [E]:" + String.format("%.4f", E));
+          //Log.e("LOG", String.valueOf(stepCount));
           handHeldFlag = false;
 
           Handler mHandler = new Handler();
@@ -296,10 +622,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
       double gyroY = event.values[1];
       double gyroZ = event.values[2];
 
+       /*
        Log.e("LOG", "GYROSCOPE           [X]:" + String.format("%.4f", event.values[0])
           + "           [Y]:" + String.format("%.4f", event.values[1])
           + "           [Z]:" + String.format("%.4f", event.values[2]));
-
+       */
       /* detect gyroZ motion when walking with hand */
       if(Math.abs(gyroZ) > handHeldZThs)
         isHandHeld = true;
@@ -323,4 +650,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
   }
 
+  private class mDirectionListener implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+      if(event.sensor.getType() == Sensor.TYPE_ORIENTATION){
+        String str;
+        int val = 0;
+        //str = "azimuth(z): "+(int)event.values[0];
+        val = (int)event.values[0];
+
+        if(isStart == true){
+          firstValue = val;
+          startValue = val;
+          isStart = false;
+        }else{
+          firstValue = lastValue;
+          lastValue = val;
+        }
+
+        int temp = lastValue;
+
+        if((temp<22.5 || temp>=337.5)){
+          dir=0;
+        }
+        else if(temp>=22.5 && temp<67.5){
+          dir=1;
+        }else if((temp>=67.5 && temp<112.5)){
+          dir=2;
+        }else if((temp>=112.5 && temp<157.5)){
+          dir=3;
+        }else if((temp>=157.5 && temp < 202.5)){
+          dir=4;
+        }else if((temp>=202.5 && temp<247.5)){
+          dir=5;
+        }else if((temp>=247.5 && temp<292.5)){
+          dir=6;
+        }else if((temp>=292.5 && temp<337.5)){
+          dir=7;
+        }
+      }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+  }
 }
